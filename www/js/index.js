833aa77718203069ea12ab7491d2f4553328640d
Tsuna-Cheung <237971264@qq.com>
2017-11-16 16:09:08 +0000

    7

--- www/js/index.js
+++ www/js/index.js
@@ -1,3 +1,27 @@
+var words = [
+    "It is certain",
+    "As I see it, yes",
+    "It is decidedly so",
+    "Most likely",
+    "Without a doubt",
+    "Outlook good",
+    "Yes definitely",
+    "Yes",
+    "You may rely on it",
+    "Signs point to yes",   
+    "Reply hazy try again",
+    "Ask again later",
+    "Better not tell you now",
+    "Cannot predict now",
+    "Concentrate and ask again",
+    "Don't count on it",
+    "My reply is no",
+    "Without a doubt", 
+    "My sources say no",
+    "Outlook not so good",
+    "Very doubtful"
+];
+
 $(document).on("pagecreate","#pageone",function(){
   $('#submitButton').on("click", function(){
     submitText();
@@ -16,10 +40,12 @@ function submitText(){
     if(rand) {
          console.log("beeb once");
         navigator.notification.beep(1);
+        navigator.vibrate(2000);
     }
     else {
         console.log("beeb twice");
         navigator.notification.beep(2);
+        navigator.vibrate(2000);
     }
     
 
